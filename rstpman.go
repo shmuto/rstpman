7d88c21b692cc2d8b6e92b0450d2d96de5ccee43
muto <dos9954@gmail.com>
2019-11-10 20:54:53 +0900

    add function to print dot1dStpPortState

--- rstpman.go
+++ rstpman.go
@@ -1,53 +1,60 @@
 package main
 
 import (
-        "fmt"
-        "log"
-		"os"
-		"time"
-        snmp "github.com/soniah/gosnmp"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	snmp "github.com/soniah/gosnmp"
 )
 
 func main() {
-		target := os.Args[1]
 
-		params := &snmp.GoSNMP{
-			Target: target,
-			Community: "public",
-			Port: 161,
-			Version: snmp.Version2c,
-			Timeout: time.Duration(1) * time.Second,
-			Logger: log.New(os.Stdout, "", 0),
-		}
+	target := &snmp.GoSNMP{
+		Target:    os.Args[1],
+		Community: "public",
+		Port:      161,
+		Version:   snmp.Version2c,
+		Timeout:   time.Duration(1) * time.Second,
+	}
 
-        err := params.Connect()
-        if err != nil {
-                log.Fatal(err)
-        }
-        defer params.Conn.Close()
+	err := target.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer target.Conn.Close()
 
-		// dot1dStpPortState ... 1.3.6.1.2.1.17.2.15.1.3
-		// ifDescr           ... 1.3.6.1.2.1.2.2.1.2
-		oids := []string{"1.3.6.1.2.1.17.2.15.1.3", "1.3.6.1.2.1.2.2.1.2"}
-		result, err := params.Get(oids)
+	// dot1dStpPortState ... 1.3.6.1.2.1.17.2.15.1.3
+	// ifDescr           ... 1.3.6.1.2.1.2.2.1.2
+	oids := []string{"1.3.6.1.2.1.17.2.15.1.3", "1.3.6.1.2.1.2.2.1.2"}
+
+	for {
+		err = target.BulkWalk(oids[0], printValue)
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		for i, variable := range result.Variables {
-			fmt.Printf("%d: oid: %s ", i, variable.Name)
-
-			// the Value of each variable returned by Get() implements
-			// interface{}. You could do a type switch...
-			switch variable.Type {
-			case snmp.OctetString:
-				fmt.Printf("string: %s\n", string(variable.Value.([]byte)))
-			default:
-				// ... or often you're just interested in numeric values.
-				// ToBigInt() will return the Value as a BigInt, for plugging
-				// into your calculations.
-				fmt.Printf("number: %d\n", snmp.ToBigInt(variable.Value))
-		}
+		time.Sleep(1 * time.Second)		
 	}
-		
+}
+
+func printValue(pdu snmp.SnmpPDU) error {
+	fmt.Printf("%s = ", pdu.Name)
+
+	switch pdu.Value {
+	case 1:
+		fmt.Println("disabled")
+	case 2:
+		fmt.Println("blocking")
+	case 3:
+		fmt.Println("listening")
+	case 4:
+		fmt.Println("learning")
+	case 5:
+		fmt.Println("forwarding")
+	case 6:
+		fmt.Println("broken")
+	}
+
+	return nil
 }
