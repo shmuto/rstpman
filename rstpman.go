217acaa5320a0eec9037d999d93e42d6395fb4c8
muto <dos9954@gmail.com>
2019-11-11 23:07:45 +0900

    probably done

--- rstpman.go
+++ rstpman.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"time"
+	"strconv"
 
 	snmp "github.com/soniah/gosnmp"
 )
@@ -19,7 +20,7 @@ func main() {
 
 	target := &snmp.GoSNMP{
 		Target:    os.Args[1],
-		Community: "public",
+		Community: os.Args[2],
 		Port:      161,
 		Version:   snmp.Version2c,
 		Timeout:   time.Duration(1) * time.Second,
@@ -31,9 +32,6 @@ func main() {
 	}
 	defer target.Conn.Close()
 
-	// dot1dStpPortState ... 1.3.6.1.2.1.17.2.15.1.3
-	oids := []string{"1.3.6.1.2.1.17.2.15.1.3", "1.3.6.1.2.1.2.2.1.2"}
-
 	for {
 		clearScreen()
 
@@ -44,60 +42,64 @@ func main() {
 			now.Hour(), now.Minute(), now.Second(),
 		)
 
-		ifList, err := getInterfaces(target)
+		ifIndexMap, err := getInterfaces(target)
 		if err != nil {
 			log.Fatal(err)
 		}
 
+		// dot1dStpPortState ... 1.3.6.1.2.1.17.2.15.1.3
+		err = target.BulkWalk("1.3.6.1.2.1.17.2.15.1.3", func(pdu snmp.SnmpPDU) error {
+			fmt.Printf("%s = ", ifIndexMap[pdu.Name[25:]])
 
+			switch pdu.Value {
+			case 1:
+				fmt.Println("disabled")
+			case 2:
+				fmt.Println("blocking")
+			case 3:
+				fmt.Println("listening")
+			case 4:
+				fmt.Println("learning")
+			case 5:
+				fmt.Println("forwarding")
+			case 6:
+				fmt.Println("broken")
+			}
 
-		err = target.BulkWalk(oids[0], printValue)
+			return nil
+		})
 		if err != nil {
 			log.Fatal(err)
 		}
-		time.Sleep(1 * time.Second)
+		time.Sleep(2 * time.Second)
 	}
 }
 
-func getInterfaces(target *snmp.GoSNMP) (map[string]int, error) {
-	ifIndexMap := map[string]int{}
+func getInterfaces(target *snmp.GoSNMP) (map[string]string, error) {
+	ifIndexMap := map[string]string{}
 
+	// Get mapping ifIndex from BRIDGE-MIB to IF-MIB
 	// dot1dBasePortIfIndex ... 1.3.6.1.2.1.17.1.4.1.2
 	err := target.BulkWalk("1.3.6.1.2.1.17.1.4.1.2", func (pdu snmp.SnmpPDU) error {
-		ifIndexMap[pdu.Name[24:]] = pdu.Value.(int)
-
+		ifIndexMap[pdu.Name[24:]] = strconv.Itoa(pdu.Value.(int))
 		return nil
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Create map from dot1dStpPortState etnry to ifName
 	//ifName ... 1.3.6.1.2.1.31.1.1.1.1 
-	err := target.BulkWalk("1.3.6.1.2.1.31.1.1.1.1", func (pdu snmp.SnmpPDU) error {
-
+	err = target.BulkWalk("1.3.6.1.2.1.31.1.1.1.1", func (pdu snmp.SnmpPDU) error {
+		for k, v := range ifIndexMap {
+			if v == pdu.Name[24:] {
+				ifIndexMap[k] = string(pdu.Value.([]uint8))
+			}
+		}
+		return nil
 	})
-	return ifIndex, nil 
-}
 
-func printValue(pdu snmp.SnmpPDU) error {
-	fmt.Printf("%s = ", pdu.Name)
-
-	switch pdu.Value {
-	case 1:
-		fmt.Println("disabled")
-	case 2:
-		fmt.Println("blocking")
-	case 3:
-		fmt.Println("listening")
-	case 4:
-		fmt.Println("learning")
-	case 5:
-		fmt.Println("forwarding")
-	case 6:
-		fmt.Println("broken")
-	}
-
-	return nil
+	return ifIndexMap, err
 }
 
 func clearScreen() {
